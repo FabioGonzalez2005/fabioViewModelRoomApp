fe79c16f66632f74763bbafa545fe850085703e2
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-12-09 23:27:03 +0100

    Creamos Favourite

--- app/src/main/java/com/example/fabioviewmodelroomapp/model/Entity.kt
+++ app/src/main/java/com/example/fabioviewmodelroomapp/model/Entity.kt
@@ -1,4 +1,22 @@
 package com.example.fabioviewmodelroomapp.model
 
-class Entity {
-}
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "favorites",
+    foreignKeys = [
+        ForeignKey(
+            entity = Marker::class,
+            parentColumns = ["id"],
+            childColumns = ["markerId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class Favorite(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val markerId: Int
+)
